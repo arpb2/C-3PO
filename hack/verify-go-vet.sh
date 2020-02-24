11c43d44966a2197c15554b6793db66c9c1459a9
Santiago Aguilera <santiago.aguilera@mercadolibre.com>
2020-02-24 14:10:40 -0300

    Refactor vet to static assertions

--- hack/verify-go-vet.sh
+++ hack/verify-go-vet.sh
@@ -4,18 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-find_files() {
-  find . -not \( \
-      \( \
-        -wholename './output' \
-        -o -wholename './.git' \
-        -o -wholename './_output' \
-        -o -wholename './_gopath' \
-        -o -wholename './release' \
-        -o -wholename './target' \
-        -o -wholename '*/third_party/*' \
-      \) -prune \
-    \) -name '*.go'
-}
-
-find_files | xargs "${go vet}" 2>&1
+go vet ./pkg/...
+go vet ./cmd/...
